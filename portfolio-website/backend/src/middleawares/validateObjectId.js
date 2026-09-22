107b8ee023ee4cfd5919b19b7ee24d00b17de8b5
DeveloperShohagMiah <themiahshohag@gmail.com>
2026-09-23 00:48:38 +0300

    chore: clean up middleware and comments

--- portfolio-website/backend/src/middleawares/validateObjectId.js
+++ portfolio-website/backend/src/middleawares/validateObjectId.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 import ApiError from "../utils/apiError.js";
 
-// Validates req.params.id (or a custom param name) is a well-formed MongoDB ObjectId.
-// Usage: router.get("/:id", validateObjectId(), getServiceById);
-//        router.get("/:projectId", validateObjectId("projectId"), getProjectById);
+
 const validateObjectId = (paramName = "id") => {
     return (req, res, next) => {
         const value = req.params[paramName];
